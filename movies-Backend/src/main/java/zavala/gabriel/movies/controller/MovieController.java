088e47a8fb69a50d4ed5da63c3b610b773e60a57
Gabozm09 <gzava004@fiu.edu>
2023-08-02 15:16:52 -0400

    added movie service support to controller

--- movies-Backend/src/main/java/zavala/gabriel/movies/controller/MovieController.java
+++ movies-Backend/src/main/java/zavala/gabriel/movies/controller/MovieController.java
@@ -1,17 +1,25 @@
 package zavala.gabriel.movies.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import zavala.gabriel.movies.Movie;
+import zavala.gabriel.movies.service.MovieService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/movies")
 
 public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
     @GetMapping
-    public ResponseEntity<String> allMovies(){
-        return new ResponseEntity<>("All Movies", HttpStatus.OK);
+    public ResponseEntity<List<Movie>> GetAllMovies(){
+        return new ResponseEntity<>(movieService.AllMovies(), HttpStatus.OK);
     }
 }
