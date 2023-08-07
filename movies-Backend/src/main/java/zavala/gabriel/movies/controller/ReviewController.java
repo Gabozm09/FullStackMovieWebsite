b4dba483a29ae11bc722d6000083b78c8faac948
Gabozm09 <gzava004@fiu.edu>
2023-08-06 22:47:36 -0400

    Created review feature

---
+++ movies-Backend/src/main/java/zavala/gabriel/movies/controller/ReviewController.java
@@ -0,0 +1,24 @@
+package zavala.gabriel.movies.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import zavala.gabriel.movies.Review;
+import zavala.gabriel.movies.service.ReviewService;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/reviews")
+public class ReviewController {
+    @Autowired
+    private ReviewService reviewService;
+
+    @PostMapping
+    public ResponseEntity<Review>createReview(@RequestBody Map<String,String> payload){
+        return new ResponseEntity<>(reviewService
+                .createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.CREATED);
+    }
+}
---
+++ movies-Backend/src/main/java/zavala/gabriel/movies/repository/ReviewRepository.java
@@ -0,0 +1,10 @@
+package zavala.gabriel.movies.repository;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import zavala.gabriel.movies.Review;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
