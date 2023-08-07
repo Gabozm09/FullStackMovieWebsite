756ce4b0d5ebc61763a430cbb9ce406614d1a09a
Gabozm09 <gzava004@fiu.edu>
2023-08-06 21:50:26 -0400

    added search single movie feature

--- movies-Backend/src/main/java/zavala/gabriel/movies/service/MovieService.java
+++ movies-Backend/src/main/java/zavala/gabriel/movies/service/MovieService.java
@@ -1,11 +1,13 @@
 package zavala.gabriel.movies.service;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import zavala.gabriel.movies.Movie;
 import zavala.gabriel.movies.repository.MovieRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -13,5 +15,9 @@ public class MovieService {
     private MovieRepository movieRepository;
     public List <Movie>AllMovies(){
         return movieRepository.findAll();
+    }
+
+    public Optional <Movie> singleMovie(String imdbId){
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 }
